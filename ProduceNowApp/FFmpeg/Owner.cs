b39c77539b3ce1b7e943eeac93bb8c2f4580405a
tweggen <timo.weggen@gmail.com>
2023-11-04 16:12:44 +0100

    - got services working in containers.

--- ProduceNowApp/FFmpeg/Owner.cs
+++ ProduceNowApp/FFmpeg/Owner.cs
@@ -39,36 +39,46 @@ public class Owner
 
     private Owner()
     {
-        string error1 = "", error2 = "";
+        string[] pathes = {
+            "/lib/x86_64-linux-gnu/",
+            "/usr/lib/x86_64-linux-gnu/",
+            "../../../../FFmpeg/ffmpeg-win/"
+        };
+        List<string> errors = new();
         bool haveIt = false;
-        if (!haveIt)
+
+        foreach (string path in pathes)
         {
+            if (!Directory.Exists(path))
+            {
+                _logger.LogInformation($"Skipping path {path}, does not exist.");
+                continue;
+            }
+
+            string pathLinux = Path.Combine(path, "libavcodec.so.58");
+            string pathWindows = Path.Combine(path, "avcodec-58.dll");
+            if (!Path.Exists(pathLinux) && !Path.Exists(pathWindows))
+            {
+                _logger.LogInformation($"Skipping path {path}, found neither {pathLinux} nor {pathWindows}.");
+                continue;
+            }
             try
             {
-                FFmpegInit.Initialise(FfmpegLogLevelEnum.AV_LOG_TRACE, "/lib/x86_64-linux-gnu/");
+                _logger.LogInformation($"Trying to load ffmpeg from {path}...");
+                
+                FFmpegInit.Initialise(FfmpegLogLevelEnum.AV_LOG_TRACE, path, _logger);
+                _logger.LogInformation("...success.");
                 haveIt = true;
+                break;
             }
             catch (Exception e)
             {
-                error1 = e.ToString();
-            }
-        }
-        if (!haveIt)
-        {
-            try
-            {
-                FFmpegInit.Initialise(FfmpegLogLevelEnum.AV_LOG_TRACE, "../../../../FFmpeg/ffmpeg-win/");
-                haveIt = true;
-            }
-            catch (Exception e)
-            {
-                error2 = e.ToString();
+                _logger.LogError($"Unable to find ffmpeg from {path}: {e}");
             }
         }
 
         if (!haveIt)
         {
-            _logger.LogError($"Unable to find ffmpeg linux libraries: {error1} and unable to find windows libraries {error2}");
             throw new InvalidOperationException("No ffmpeg found.");
         }
     }
